7382d0e23b38ae3b86e57b371b2b9b9a07c6f7e4
Steinway Wu <steinwaywhw@gmail.com>
2012-11-11 14:27:18 -0500

    Multithreaded file loader now available

---
+++ file_loader.h
@@ -0,0 +1,22 @@
+#include "bool.h"
+
+#define MAXFILES 128
+#define MAXLENGTH 128
+
+typedef struct file_loader_t {
+	char prefix[MAXLENGTH];
+	char filenames[MAXFILES][MAXLENGTH];
+	char fullfilenames[MAXFILES][MAXLENGTH];
+	int *descriptors;
+	int count;
+
+	bool (*exists) (struct file_loader_t *loader, char *filename);
+	int (*open) (struct file_loader_t *loader, char *filename);
+	int (*read) (struct file_loader_t *loader, int fd, int offset, int count, void *buffer);
+} file_loader_t;
+
+int init_file_loader (file_loader_t *loader, int count, char *prefix, char **filenames);
+int destroy_file_loader (file_loader_t *loader);
+
+#define ENOTFOUND -1
+#define EOF 0
